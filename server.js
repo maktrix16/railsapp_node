70ff4b921243d952258474bc5b9c21c9434e25d8
Arthur Mak <arthur.t.mak@gmail.com>
2015-02-12 12:56:08 -0800

    first node commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var express=require('express');
+var path=require('path');
+var bodyParser=require('body-parser');
+var app=express();
+
+// app.use(express.static(path.join(__dirname, "./static")));
+// app.use(bodyParser.urlencoded());
+// app.set('views', path.join(__dirname, './views'));
+// app.set('view engine','ejs');
+
+// app.get('/',function(req,res){
+// 	res.render('index');
+// });
+
+var server = app.listen(8000,function(){
+	console.log('listening to port 8000');
+});
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection',function(socket){
+
+	socket.on('emit_new_user',function(){
+		console.log(socket.id);
+	});
+
+	socket.on('message_emit',function(data){
+		console.log(data);
+		socket.broadcast.emit('message_broadcast',data);
+	});
+
+});
